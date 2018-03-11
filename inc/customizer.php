db2b56acb0fcebd266c33060d9c5bb8f128c4f21
Theme Egg <themeeggofficial@gmail.com>
2018-03-11 13:43:39 +0545

    important links added

---
+++ inc/customizer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Miteri Theme Customizer.
+ *
+ * @package Theme Egg
+ * @subpackage Miteri
+ * @since 1.0.0
+ */
+
+
+require get_template_directory() . '/inc/customizer/parts/miteri-important-link-panel.php'; //Important Link panel
---
+++ inc/customizer/miteri-sanitize.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Define function about sanitation for customizer option
+ *
+ * @package Theme Egg
+ * @subpackage Miteri
+ * @since 1.0.0
+ */
+//Text
+
+function miteri_links_sanitize() {
+	return false;
+}
