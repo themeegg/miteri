3ce107ab17fe0c48a09a641fb655461273839725
Theme Egg <themeeggofficial@gmail.com>
2018-04-11 19:51:46 +0545

    customizer issue fix

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -582,7 +582,9 @@ function miteri_theme_customizer( $wp_customize ) {
 
      $wp_customize->add_setting('website_skin', array(
         'default' => 'no_skin',
-    ));
+        'sanitize_callback' => 'sanitize_miteri_website_skin',
+
+     ));
 
     $wp_customize->add_control('website_skin', array(
         'label' => esc_html__('Website Skin', 'miteri'),
@@ -648,6 +650,17 @@ function sanitize_miteri_website_layout( $input, $setting ) {
     } else {
         return $setting->default;
     }
+}
+function sanitize_miteri_website_skin( $input, $setting ) {
+	global $wp_customize;
+
+	$control = $wp_customize->get_control( $setting->id );
+
+	if ( array_key_exists( $input, $control->choices ) ) {
+		return $input;
+	} else {
+		return $setting->default;
+	}
 }
 
 /**
